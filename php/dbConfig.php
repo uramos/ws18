087f025065156940bfc2092b924736753cfd77f6
Unai Ramos <unai5ramos@gmail.com>
2018-10-17 15:12:16 +0200

    berriro saiatzen

---
+++ php/dbConfig.php
@@ -0,0 +1,18 @@
+<?php
+
+$lokal=0; // hodeian 0, lokalean gauedenean 1
+
+
+if ($lokal) {
+   $zerbitzaria="localhost";
+   $erabiltzailea="id7195700_urafe";
+   $gakoa="contraseña";  // GitHub-en eremu hau EZABATU
+   $db="id7195700_quiz";        // hodeiko db izena: hodeiko aurrizkia + zuek adierazitako db izena atzizki moduan
+
+} else {
+   $zerbitzaria="localhost";
+   $erabiltzailea="root";   // lokalean erabiltzailea root izan ohi da
+   $gakoa="";               // eta ez da pasahitzarik jartzen
+   $db="quiz";
+}
+?>
