b8314e7088a1020f13de0dfd03d055d6a4b874b9
Unai Ramos <unai5ramos@gmail.com>
2018-10-18 11:35:34 +0200

    laboan lanean

--- php/addQuestion.php
+++ php/addQuestion.php
@@ -1,26 +1,26 @@
 <?php
-include("dbConfig.php");
+include 'dbConfig.php';
 
 #hau ere lerro bateko iruzkina
-/*Oraingoan zenbait lerrotan barrena
+/*Oraingoan zenbait lerrotan barrena 
 Luzatzen da, argi dago? */
 
-$link= new mysqli ($zerbitzaria,$erabiltzailea,$gakoa, $db,);
+$link= new mysqli($zerbitzaria, $erabiltzailea, $gakoa, $db);
 
 if($link->connect_errno) {
-	die( "Huts egin du konexioak MySQL-ra: (".$link-> connect_errno ") " .$link-> connect_error);
+	die( "Huts egin du konexioak MySQL-ra: (". $link-> connect_errno .") " .$link-> connect_error);
 }
 echo "Konexioa egin da:" . $link->host_info;
 
-$sql = "INSERT INTO Questions (eMail, sarGaldera, sarZuzena, sarOkerra1, sarOkerra2, sarOkerra3, galderaZail, galderaGai) 
+$sql = "INSERT INTO questions (eMail, sarGaldera, sarZuzena, sarOkerra1, sarOkerra2, sarOkerra3, galderaZail, galderaGai) 
 VALUES ('$_POST[posta]', '$_POST[sarGaldera]', '$_POST[sarZuzena]', '$_POST[sarOkerra1]', '$_POST[sarOkerra2]', '$_POST[sarOkerra3]', '$_POST[galderaZail]', '$_POST[galderaGai]')";
 
 $ema=mysqli_query($link,$sql);
 
 if(!$ema){
-	die('Errorea query-a gauzatzerakoan: ' .mysqli_error());
+	die('Errorea query-a gauzatzerakoan: ' . mysqli_error());
 }
 
 echo "Erregistro bat gehitu da.";
-
+ 
 mysqli_close($link);
--- php/dbConfig.php
+++ php/dbConfig.php
@@ -1,18 +1,17 @@
 <?php
 
-$lokal=0; // hodeian 0, lokalean gauedenean 1
+$lokal=1; // hodeian 0, lokalean gauedenean 1
 
 
-if ($lokal) {
-   $zerbitzaria="localhost";
-   $erabiltzailea="id7195700_urafe";
-   $gakoa="contraseña";  // GitHub-en eremu hau EZABATU
-   $db="id7195700_quiz";        // hodeiko db izena: hodeiko aurrizkia + zuek adierazitako db izena atzizki moduan
-
-} else {
+if ($lokal){
    $zerbitzaria="localhost";
    $erabiltzailea="root";   // lokalean erabiltzailea root izan ohi da
    $gakoa="";               // eta ez da pasahitzarik jartzen
    $db="quiz";
-}
+} else{
+   $zerbitzaria="localhost";
+   $erabiltzailea="id7195700_urafe";
+   $gakoa="contraseña";  // GitHub-en eremu hau EZABATU
+   $db="id7195700_quiz";        // hodeiko db izena: hodeiko aurrizkia + zuek adierazitako db izena atzizki moduan
+} 
 ?>
